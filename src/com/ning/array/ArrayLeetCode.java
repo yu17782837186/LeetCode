99814185c83bd8f1706cec010a866ab179f68d20
HASEE <yu17782837186@163.com>
2021-03-15 17:42:53 +0800

    2021/3/15 leetcode

--- src/com/ning/array/ArrayLeetCode.java
+++ src/com/ning/array/ArrayLeetCode.java
@@ -370,4 +370,29 @@ public class ArrayLeetCode {
         }
         return result;
     }
+    /**
+     * time:2021/3/15  16. 最接近的三数之和
+     给定一个包括 n 个整数的数组 nums 和 一个目标值 target。找出 nums 中的三个整数，使得它们的和与 target 最接近。返回这三个数的和。假定每组输入只存在唯一答案。
+     时间复杂度o(n2) 空间复杂度o(1)
+     */
+    public int threeSumClosest(int[] nums, int target) {
+        Arrays.sort(nums);
+        int ans = nums[0] + nums[1] + nums[2];
+        for(int i=0;i<nums.length;i++) {
+            int start = i+1, end = nums.length - 1;
+            while(start < end) {
+                int sum = nums[start] + nums[end] + nums[i];
+                if(Math.abs(target - sum) < Math.abs(target - ans))
+                    ans = sum;
+                if(sum > target)
+                    end--;
+                else if(sum < target)
+                    start++;
+                else
+                    return ans;
+            }
+        }
+        return ans;
+    }
+
 }
