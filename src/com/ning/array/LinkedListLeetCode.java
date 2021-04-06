05615308e0a3a426ffc5c73e86cd8f5fda4ffa15
HASEE <yu17782837186@163.com>
2021-04-06 16:59:06 +0800

    leetcode

--- src/com/ning/array/LinkedListLeetCode.java
+++ src/com/ning/array/LinkedListLeetCode.java
@@ -1,7 +1,19 @@
 package com.ning.array;
 
 public class LinkedListLeetCode {
-    public  ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+    class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+    /**
+     time:2021/4/2  21. 合并两个有序链表
+     将两个升序链表合并为一个新的 升序 链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。
+     递归
+     */
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
         if(l1 == null) {
             return l2;
         }else if(l2 == null) {
@@ -14,21 +26,23 @@ public class LinkedListLeetCode {
             return l2;
         }
     }
-}
-
-class ListNode {
-    int val;
-    ListNode next;
-
-    ListNode() {
-    }
-
-    ListNode(int val) {
-        this.val = val;
-    }
-
-    ListNode(int val, ListNode next) {
-        this.val = val;
-        this.next = next;
+    /**
+     time:2021/4/6  83. 删除排序链表中的重复元素
+     存在一个按升序排列的链表，给你这个链表的头节点 head ，请你删除所有重复的元素，使每个元素 只出现一次 。
+     返回同样按升序排列的结果链表。
+     */
+    public ListNode deleteDuplicationElement(ListNode listNode) {
+        if(listNode == null) {
+            return listNode;
+        }
+        ListNode cur = listNode;
+        while(cur.next != null) {
+            if(cur.val == cur.next.val) {
+                cur.next = cur.next.next;
+            }else {
+                cur = cur.next;
+            }
+        }
+        return listNode;
     }
 }
