ea9fe324518657f6fbacc390f275141dc71518f4
HASEE <yu17782837186@163.com>
2021-04-13 16:56:22 +0800

    leetcode

--- src/com/ning/array/LinkedListLeetCode.java
+++ src/com/ning/array/LinkedListLeetCode.java
@@ -254,4 +254,13 @@ public class LinkedListLeetCode {
         }
         return slow;
     }
+    /**
+     time:2021/4/13  237. 删除链表中的节点
+     请编写一个函数，使其可以删除某个链表中给定的（非末尾）节点。传入函数的唯一参数为 要被删除的节点 。
+     */
+    //时间复杂度和空间复杂度都是o(1)
+    public void deleteNode(ListNode head) {
+        head.val = head.next.val;
+        head.next = head.next.next;
+    }
 }
